2a114e6ada2ddc93c6063880a7bab9160b48fea9
KJee26 <1406607599@qq.com>
2020-10-30 17:32:09 +0800

    second

--- src/web/servlet/LoginServlet.java
+++ src/web/servlet/LoginServlet.java
@@ -2,6 +2,7 @@ package web.servlet;
 
 import dao.UserDao;
 import domain.User;
+import org.apache.commons.beanutils.BeanUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 @WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
@@ -18,14 +21,27 @@ public class LoginServlet extends HttpServlet {
         //1.设置编码
         request.setCharacterEncoding("utf-8");
 
-        //2.获取请求参数
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+//        //2.获取请求参数
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//
+//        //3.封装user对象
+//        User loginUser = new User();
+//        loginUser.setUsername(username);
+//        loginUser.setPassword(password);
 
-        //3.封装user对象
+        //2.获取所有请求参数
+        Map<String, String[]> map = request.getParameterMap();
+        //3.1创建user对象
         User loginUser = new User();
-        loginUser.setUsername(username);
-        loginUser.setPassword(password);
+        //3.2使用BeanUtils封装
+        try {
+            BeanUtils.populate(loginUser, map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
 
         //4.调用UserDao的login方法
         UserDao dao = new UserDao();
