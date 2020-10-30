0f3a91d4c9aa72ae84655e33b4b0a78f16c0e13e
KJee26 <1406607599@qq.com>
2020-10-30 16:48:39 +0800

    first commit

---
+++ src/dao/UserDao.java
@@ -0,0 +1,29 @@
+package dao;
+
+import domain.User;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import util.JDBCUtils;
+
+public class UserDao {
+
+    //声明JDBCTemplate对象共用
+    private JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
+
+    //登录方法
+    public User login(User loginUser){
+        try {
+            //1.编写sql
+            String sql = "select * from user where username=? and password=?";
+            //2.调用query方法
+            User user = template.queryForObject(sql,
+                    new BeanPropertyRowMapper<User>(User.class),
+                    loginUser.getUsername(), loginUser.getPassword());
+            return user;
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
